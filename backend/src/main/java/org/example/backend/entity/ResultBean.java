ada134b271def3a66f171f1a0deb8ad5d855566e
changxuedeng <dengchangxue@qq.com>
2024-02-15 20:37:54 +0800

    后端：配置Security

---
+++ backend/src/main/java/org/example/backend/entity/ResultBean.java
@@ -0,0 +1,43 @@
+package org.example.backend.entity;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+public class ResultBean<T> {
+    private int code;
+    private T data;
+    private String message;
+
+    ResultBean(int code, T data){
+        this.code = code;
+        this.data = data;
+    }
+    ResultBean(T data){
+        this.data = data;
+    }
+    ResultBean(T data, String message){
+        this.data = data;
+        this.message = message;
+    }
+
+    public static <T> ResultBean<T> success(T data, String message){
+        return new ResultBean<T>(200,data,message);
+    }
+    public static <T> ResultBean<T> success(String message){
+        return new ResultBean<T>(200,null,message);
+    }
+    public static <T> ResultBean<T> failure(int code, T data, String message){
+        return new ResultBean<T>(code, data, message);
+    }
+    public static <T> ResultBean<T> failure(int code, String message){
+        return new ResultBean<T>(code, null, message);
+    }
+    public String asJOSNString(){
+        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
+    }
+}
